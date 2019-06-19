d285f7063c9d3fe0783c88c7c1bc411349fba3f5
Nathan Tavish Reynolds <ntreyn@umich.edu>
2019-06-19 13:51:14 -0400

    filled piece actions except castling, en passant, last row promotion

--- env.py
+++ env.py
@@ -42,6 +42,10 @@ class chess_env:
         ]
         self.done = False
         self.player = 'W'
+        self.black_remaining = [-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16]
+        self.black_lost = []
+        self.white_remaining = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]
+        self.white_lost = []
 
     def step(self, action):
         """
@@ -49,8 +53,10 @@ class chess_env:
         piece : letter+number(optional)
         tile : letter+number -> (row, col)
         """
-        piece = self.translate_piece(action[0])
-        tile = self.translate_tile(action[1])
+        piece = self.correct_piece(action[0])
+        tile = self.tile_to_coord(action[1])
+
+        print(self.get_moves())
 
 
 
@@ -106,13 +112,13 @@ class chess_env:
                 if tile != ' ':
                     self.piece_locations[tile] = (r, c)
 
-    def translate_piece(self, piece):
+    def correct_piece(self, piece):
         if self.player == 'W':
             return piece.upper()
         else:
             return piece.lower()
 
-    def translate_tile(self, tile):
+    def tile_to_coord(self, tile):
         letter = tile[0].upper()
         number = int(tile[1])
 
@@ -132,8 +138,18 @@ class chess_env:
 
         return (row, col)
 
+    def coord_to_tile(self, r, c):
+        col_to_letter = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+        letter = col_to_letter[c]
+        number = 8 - r
+        return letter + str(number)    
+
     def can_capture(self, p1, p2):
-        return p1.islower() != p2.islower()
+        prod = p1 * p2
+        if prod < 0:
+            return True
+        else:
+            return False
 
     def in_bounds(self, r, c):
         if r > 7 or r < 0 or c > 7 or c < 0:
@@ -143,6 +159,7 @@ class chess_env:
 
     def step_checker(self, row_offset, col_offset, tile, piece):
         r, c = tile
+        moves = []
 
         while True:
             r += row_offset
@@ -151,16 +168,19 @@ class chess_env:
             if not self.in_bounds(r, c):
                 break
 
-            if self.board[r][c] == ' ':
+            if self.board[r][c] == 0:
                 # Open square, possible move
-                pass
+                moves.append((r, c))
+                
             elif self.can_capture(piece, self.board[r][c]):
                 # Enemy piece, possible move
+                moves.append((r, c))
                 break
             else:
                 # Friendly piece, impossible move
                 break
-
+        
+        return moves
 
     def num_attacking(self, tile):
         # Number of enemy pieces attacking square
@@ -170,64 +190,119 @@ class chess_env:
         # Number of friendly pieces defending square
         pass
 
+    def get_moves(self):
+        raw_moves = self.potential_moves()
+        moves = []
+
+        for p, c in raw_moves:
+            tile = self.coord_to_tile(c[0], c[1])
+            piece = self.ids_to_pieces[p]
+            moves.append((piece, tile))
+        
+        return moves
+
+    def potential_moves(self):
+        if self.player == 'W':
+            pieces = self.white_remaining
+        else:
+            pieces = self.black_remaining
+
+        moves = []
+
+        for p in pieces:
+            abs_p = abs(p)
+            if abs_p >= 9:
+                pawn_moves = self.pawn_actions(p)
+                for m in pawn_moves:
+                    moves.append((p, m))
+            elif abs_p == 1 or abs_p == 8:
+                rook_moves = self.rook_actions(p)
+                for m in rook_moves:
+                    moves.append((p, m))
+            elif abs_p == 2 or abs_p == 7:
+                knight_moves = self.knight_actions(p)
+                for m in knight_moves:
+                    moves.append((p, m))
+            elif abs_p == 3 or abs_p == 6:
+                bishop_moves = self.bishop_actions(p)
+                for m in bishop_moves:
+                    moves.append((p, m))
+            elif abs_p == 4:
+                queen_moves = self.queen_actions(p)
+                for m in queen_moves:
+                    moves.append((p, m))
+            elif abs_p == 5:
+                king_moves = self.king_actions(p)
+                for m in king_moves:
+                    moves.append((p, m))
+            else:
+                # Should never happen
+                pass
+    
+        return moves
+
     def pawn_actions(self, pawn):
 
         row, col = self.piece_locations[pawn]
+        moves = []
 
-        if pawn[0] == 'P':
+        if pawn > 0:
             not_last = row != 0
             not_left_last = col != 0 and not_last
             not_right_last = col != 7 and not_last
             
-            if row == 6 and self.board[5][col] == ' ' and self.board[4][col] == ' ':
+            if row == 6 and self.board[5][col] == 0 and self.board[4][col] == 0:
                 # Starting Double Move Forward
-                pass
-            if not_last and self.board[row - 1] == ' ':
+                moves.append((4, col))
+            if not_last and self.board[row - 1][col] == 0:
                 # Single Move Forward
-                pass
+                moves.append((row - 1, col))
             if not_left_last and self.can_capture(pawn, self.board[row - 1][col - 1]):
                 # Diagonal capture left
-                pass
+                moves.append((row - 1, col - 1))
             if not_right_last and self.can_capture(pawn, self.board[row - 1][col + 1]):
                 # Diagonal capture right
-                pass
+                moves.append((row - 1, col + 1))
             
             # En Passant
             # TODO
 
-            if row == 1 and self.board[row - 1] == ' ':
+            if row == 1 and self.board[row - 1][col] == 0:
                 # Last Row Promotion
+                # TODO
                 pass
 
-        elif pawn[0] == 'p':
+        elif pawn < 0:
             not_last = row != 7
             not_left_last = col != 0 and not_last
             not_right_last = col != 7 and not_last
             
-            if row == 1 and self.board[2][col] == ' ' and self.board[3][col] == ' ':
+            if row == 1 and self.board[2][col] == 0 and self.board[3][col] == 0:
                 # Starting Double Move Forward
-                pass
-            if not_last and self.board[row + 1] == ' ':
+                moves.append((3, col))
+            if not_last and self.board[row + 1][col] == 0:
                 # Single Move Forward
-                pass
+                moves.append((row + 1, col))
             if not_left_last and self.can_capture(pawn, self.board[row + 1][col - 1]):
                 # Diagonal capture left
-                pass
+                moves.append((row + 1, col - 1))
             if not_right_last and self.can_capture(pawn, self.board[row + 1][col + 1]):
                 # Diagonal capture right
-                pass
+                moves.append((row + 1, col + 1))
             
             # En Passant
             # TODO
 
-            if row == 6 and self.board[row + 1] == ' ':
+            if row == 6 and self.board[row + 1][col] == 0:
                 # Last Row Promotion
+                # TODO
                 pass
         
         else:
             # Should never happen
             pass
 
+        return moves
 
     def rook_actions(self, rook):
 
@@ -235,13 +310,17 @@ class chess_env:
         offsets = [
             (1, 0), (0, 1), (-1, 0), (0, -1)
         ]
+        moves = []
 
         for r, c in offsets:
-            self.step_checker(r, c, rook_tile, rook)
+            steps = self.step_checker(r, c, rook_tile, rook)
+            for s in steps:
+                moves.append(s)
 
         # Castling
         # TODO
 
+        return moves
 
     def knight_actions(self, knight):
         """
@@ -252,24 +331,23 @@ class chess_env:
             4       5   
         """
 
-        # 8 'L' Jumps
-
         row, col = self.piece_locations[knight]
-
         jumps = [
             (row - 1, col - 2), (row - 2, col - 1),
             (row - 1, col + 2), (row - 2, col + 1),
             (row + 1, col - 2), (row + 2, col - 1),
             (row + 1, col + 2), (row + 2, col + 1)
         ]
+        moves = []
 
         for r, c in jumps:
             if self.in_bounds(r, c):
                 tile = self.board[r][c]
-                if tile == ' ' or self.can_capture(knight, tile):
+                if tile == 0 or self.can_capture(knight, tile):
                     # Possible move
-                    pass
-
+                    moves.append((r, c))
+        
+        return moves
 
     def bishop_actions(self, bishop):
 
@@ -277,9 +355,14 @@ class chess_env:
         offsets = [
             (1, 1), (1, -1), (-1, 1), (-1, -1)
         ]
+        moves = []
 
         for r, c in offsets:
-            self.step_checker(r, c, bish_tile, bishop)
+            steps = self.step_checker(r, c, bish_tile, bishop)
+            for s in steps:
+                moves.append(s)
+
+        return moves
 
     def queen_actions(self, queen):
 
@@ -288,27 +371,34 @@ class chess_env:
             (1, 0), (0, 1), (-1, 0), (0, -1),
             (1, 1), (1, -1), (-1, 1), (-1, -1)
         ]
+        moves = []
 
         for r, c in offsets:
-            self.step_checker(r, c, queen_tile, queen)
+            steps = self.step_checker(r, c, queen_tile, queen)
+            for s in steps:
+                moves.append(s)
+
+        return moves
 
     def king_actions(self, king):
 
         row, col = self.piece_locations[king]
-
-        moves = [
+        spaces = [
             (row - 1, col - 1), (row + 1, col + 1),
             (row - 1, col + 1), (row + 1, col - 1),
             (row + 1, col), (row - 1, col),
             (row, col + 1), (row, col - 1)
         ]
+        moves = []
 
-        for r, c in moves:
+        for r, c in spaces:
             if self.in_bounds(r, c):
                 tile = self.board[r][c]
-                if self.num_attacking((r,c)) == 0 and (tile == ' ' or self.can_capture(king, tile)):
+                if self.num_attacking((r,c)) == 0 and (tile == 0 or self.can_capture(king, tile)):
                     # Possible move
-                    pass
+                    moves.append((r, c))
+        
+        return moves
 
         
 
